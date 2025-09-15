545d65ed6f2dfa7995ad0b9a5c756c3d7f1099a6
Harendra Kumar Shah <harendra.shah@pdc01.lisnepal.com.np>
2025-09-15 22:24:19 +0545

    basic part cover

--- lab4_string_repeater (1).py
+++
@@ -1,7 +0,0 @@
-def intreger(intr):
-    multiplying= intr*3
-    print("the multiplying it by an integer: ",str(multiplying))
-def main():
-    intr=str(input("entre the name "))
-    intreger(intr)
-main()
